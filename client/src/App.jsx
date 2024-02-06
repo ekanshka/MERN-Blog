a95e3ea97dfd06c40009881459a94e342d825e29
Ekansh <efuforofficial@gmail.com>
2024-02-06 22:05:23 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      hi
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
