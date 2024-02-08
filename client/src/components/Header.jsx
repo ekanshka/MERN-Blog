f2f61666d496450714ce0985c397435c86bd6ba0
Ekansh <efuforofficial@gmail.com>
2024-02-09 03:06:11 +0530

    created navbar with flowbite react

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import { Button, Navbar, NavbarLink, NavbarToggle, TextInput } from 'flowbite-react'
+import React from 'react'
+import { Link, useLocation} from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon } from 'react-icons/fa'
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className='border-b-2'>
+      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-blue-950  via-purple-700 to-pink-500 rounded-lg text-white'>Jaya's</span>Blog
+      </Link>
+      {/* via-indigo-800 */}
+      <form>
+        <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline'/>
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link to='/sign-in'>
+          <Button outline gradientDuoTone='purpleToBlue'>
+            Sign In
+          </Button>
+        </Link>
+      </div>
+      <NavbarToggle />
+      <Navbar.Collapse >
+        <Navbar.Link active={path === '/'} as={'div'} >
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <NavbarLink active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </NavbarLink>
+        <NavbarLink active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </NavbarLink>
+      </Navbar.Collapse>
+
+    </Navbar>
+  )
+}
