447d866caf6e17459fbd5729cc0f550618a583f7
Ekansh <efuforofficial@gmail.com>
2024-02-10 02:25:41 +0530

    tried some bigger fonts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,62 @@
-import { Button, Navbar, NavbarLink, NavbarToggle, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link, useLocation} from 'react-router-dom'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import {
+  Button,
+  Navbar,
+  NavbarLink,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
   const path = useLocation().pathname;
   return (
-    <Navbar className='border-b-2'>
-      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-blue-950  via-purple-700 to-pink-500 rounded-lg text-white'>Jaya's</span>Blog
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-md sm:text-2xl font-semibold dark:text-white"
+      >
+        <span className="px-3 py-1 bg-gradient-to-r from-blue-950  via-purple-700 to-pink-500 rounded-lg text-white">
+          Jaya's
+        </span>
+        Blog
       </Link>
       {/* via-indigo-800 */}
       <form>
-        <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline'/>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button outline gradientDuoTone='purpleToBlue'>
+        <Link to="/sign-in">
+          <Button outline gradientDuoTone="purpleToBlue">
             Sign In
           </Button>
         </Link>
       </div>
       <NavbarToggle />
-      <Navbar.Collapse >
-        <Navbar.Link active={path === '/'} as={'div'} >
-          <Link to='/'>Home</Link>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <NavbarLink active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+        <NavbarLink active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </NavbarLink>
-        <NavbarLink active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+        <NavbarLink active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
         </NavbarLink>
       </Navbar.Collapse>
-
     </Navbar>
-  )
+  );
 }
